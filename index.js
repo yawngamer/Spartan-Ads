3b294c63c10e145d3a9a43655b6bd36f40a04239
Quincy <55165281+qstrange23@users.noreply.github.com>
2019-10-01 17:02:55 -0700

    Testing a thing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ gapi.load('auth2', function() {
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   sessionStorage.setItem('Name', profile.getName());
-  sessionStorage.setItem('Email,', profile.getEmail());
+  sessionStorage.setItem('Email', profile.getEmail());
+  sessionStorage.setItem('Email', 'lol gottem');
   window.location.replace("http://ads.lciteam.club/welcome.html");
 }
