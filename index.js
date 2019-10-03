5d421832675c79285b1feb93decd34e2f5001bd9
Quincy <55165281+qstrange23@users.noreply.github.com>
2019-10-03 15:38:11 -0700

    did a thing

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ gapi.load('auth2', function() {
   gapi.auth2.int();
 });
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  sessionStorage.setItem('Name', profile.getName());
-  sessionStorage.setItem('Email', profile.getEmail());
-  sessionStorage.setItem('Email', 'lol gottem');
   window.location.replace("http://ads.lciteam.club/welcome.html");
 }
