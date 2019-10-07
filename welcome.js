fdd383d6bbecb6df7e673f69aa21847f6f925786
RoblKyogre‮ <42159204+RoblKyogre@users.noreply.github.com>
2019-10-07 13:06:02 -0700

    did another thing
    ask quincy

--- welcome.js
+++ welcome.js
@@ -1,3 +1,6 @@
+gapi.load('auth2', function() {
+  gapi.auth2.int();
+});
 var profile = googleUser.getBasicProfile();
 if (profile.getEmail() === null) {
   window.location.replace("http://ads.lciteam.club/");
